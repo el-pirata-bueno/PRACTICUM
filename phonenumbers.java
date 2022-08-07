5f7eac766a22d6de568ff95e7f0405bddcd1a077
el-pirata-bueno <110718064+el-pirata-bueno@users.noreply.github.com>
2022-08-07 21:11:45 +0300

    Update phonenumbers.java

--- phonenumbers.java
+++ phonenumbers.java
@@ -18,13 +18,15 @@ CALL-1-ME = 2255163
  */
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class phonenumbers
 {
    public static void main(String[] args)
    {
-		String[][] symbols = { 								// создаём массив исходных данных
+		String[][] symbols = { 									// создаём массив исходных данных
+			{"0", "0"},
+			{"1", "1"},
 			{"2", "A", "B", "C"},
 			{"3", "D", "E", "F"},
 			{"4", "G", "H", "I"},
@@ -34,10 +36,21 @@ public class phonenumbers
 			{"8", "T", "U", "V"},
 			{"9", "W", "X", "Y", "Z"},
 		};
-
-		HashMap<String, String> map = new HashMap<>(); 		// создаём словарь из исходных данных: ключ - символ, значение - цифра
 		
-		for (int i = 0; i < symbols.length; i++) { 			// заполняем словарь парамми буква-цифра
+		int numbers = 0;
+		
+		HashMap<String, String> map = new HashMap<>(); 			// создаём словарь из исходных данных: ключ - символ, значение - цифра
+		
+		HashSet<String> ignoredSymbols = new HashSet<String>();	// создаем и заполняем множество игнорируемых символов при вводе пользователя: +, -, (, ), пробел
+		ignoredSymbols.add("+");
+		ignoredSymbols.add("-");
+		ignoredSymbols.add(" ");
+		ignoredSymbols.add("(");
+		ignoredSymbols.add(")");
+		
+		StringBuilder phoneNumberFinal = new StringBuilder("");
+		
+		for (int i = 0; i < symbols.length; i++) { 				// заполняем словарь парами буква-цифра
 			for (int j = 1; j < symbols[i].length; j++) {
 				map.put(symbols[i][j], symbols[i][0]);
 			}
@@ -45,33 +58,37 @@ public class phonenumbers
 		
 		Scanner scanner = new Scanner(System.in); 			
 		System.out.println("Введите номер телефона (допустимые символы: +, -, (, ), 0...9, A...Z)");
-		String phoneNumberFinal = "";
 		
-		while (phoneNumberFinal == "") {
-			String phoneNumberStart = scanner.nextLine();	// считываем номер телефона с консоли
+		while (numbers == 0) {
+			String phoneNumberStart = scanner.nextLine();						// считываем номер телефона с консоли
 			
-			for (int k = 0; k < phoneNumberStart.length(); k++) { 	// проверяем поочередно каждый введенный символ и сравниваем его с условиями
-				String checkedSymbol = String.valueOf(phoneNumberStart.charAt(k)); 		
+			for (int k = 0; k < phoneNumberStart.length(); k++) { 				// проверяем поочередно каждый введенный символ и сравниваем его с условиями
+				String checkedSymbol = String.valueOf(phoneNumberStart.charAt(k)).toUpperCase(); 		
 				
-				if ((checkedSymbol.equals("+")) || (checkedSymbol.equals("-")) || (checkedSymbol.equals("(")) || (checkedSymbol.equals(")"))) {
-					phoneNumberFinal = phoneNumberFinal + "";
-				}
-				
-				else if ((checkedSymbol.equals("0"))  || (checkedSymbol.equals("1")) || map.containsValue(checkedSymbol)) { // здесь для сокращения кода, проверяем, нет ли символа в значениях словаря
-					phoneNumberFinal = phoneNumberFinal + checkedSymbol;
+				if (map.containsValue(checkedSymbol)) { 						// условие для введенной цифры
+					phoneNumberFinal.append(checkedSymbol);
 				}	
 				
-				else if (map.containsKey(checkedSymbol)) {			// условия для введенной буквы - заменяем на цифру
-					phoneNumberFinal = phoneNumberFinal + map.get(checkedSymbol);
+				else if (map.containsKey(checkedSymbol)) {						// условие для введенной буквы - заменяем на цифру
+					phoneNumberFinal.append(map.get(checkedSymbol));
 				}
 				
-				else {
+				else if (ignoredSymbols.contains(checkedSymbol)) {				// условие для игнорируемых символов
+				}
+				
+				else {															// условие для ошибочного символа
 					System.out.println("Ошибка! Повторите ввод. Допустимые символы: +, -, (, ), 0...9, A...Z)");
-					phoneNumberFinal = "";
+					phoneNumberFinal.delete(0, k);
+					numbers = 0;
 					break;
-				}	
+				}
+			numbers++;
 			}
+			if (numbers != 0) {
+				break;
+			}			
 		}
-		System.out.println("Номер телефона в выбранном формате: " + phoneNumberFinal);
+		System.out.println("Номер телефона в выбранном формате: " + phoneNumberFinal);		
 	}		
 }
+
